f27401067ccbff4a463890e185bd2ca8f7a9dec3
Grayton Ward <g-buddy@comcast.net>
2019-02-01 16:41:36 -0500

    Path error fix

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ scheduler.scheduleJob('0 * * * *', function() {
 //#region storageReport
 app.post(storageReportUrl, function(req, res){
   shell.exec('./storageReport');
-  res.sendFile('output.log');
+  res.sendFile(__dirname + 'src/output.log');
 });
 
 //#endregion
